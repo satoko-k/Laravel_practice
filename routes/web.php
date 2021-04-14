e871e47144aefdc796c84de5a8e89a38d3985735
satoko-k <80142169+satoko-k@users.noreply.github.com>
2021-04-15 01:13:54 +0900

    練習

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+use App\Book;
+use Illuminate\Http\Request;
+
+/**
+* 本の一覧表示(books.blade.php)
+*/
+Route::get('/', function () {
+    return view('books');
+});
+
+/**
+* 本を追加 
+*/
+Route::post('/books', function (Request $request) {
+    //
+});
+
+/**
+* 本を削除 
+*/
+Route::delete('/book/{book}', function (Book $book) {
+    //
+});
+
+
+?>
