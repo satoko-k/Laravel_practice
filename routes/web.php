502fc0fbf8188b13569edcb010bae2ea2f0bd8c5
satoko-k <80142169+satoko-k@users.noreply.github.com>
2021-04-16 05:49:59 +0900

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -22,21 +22,50 @@ use Illuminate\Http\Request;
 * 本の一覧表示(books.blade.php)
 */
 Route::get('/', function () {
-    return view('books');
+    // return view('books');
+    
+    $books = Book::orderBy('created_at', 'asc')->get();
+    return view('books', [
+        'books' => $books
+    ]);
 });
 
 /**
 * 本を追加 
 */
 Route::post('/books', function (Request $request) {
+    // dd( $request );
+    
     //
+    //バリデーション
+    $validator = Validator::make($request->all(), [
+        'item_name' => 'required|max:255',
+    ]);
+
+    //バリデーション:エラー 
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    
+     // Eloquentモデル（登録処理）
+    $books = new Book;
+    $books->item_name = $request->item_name;
+    $books->item_number = '1';
+    $books->item_amount = '1000';
+    $books->published = '2017-03-07 00:00:00';
+    $books->save(); 
+    return redirect('/');
+    
 });
 
 /**
 * 本を削除 
 */
 Route::delete('/book/{book}', function (Book $book) {
-    //
+    $book->delete();
+    return redirect('/');  
 });
 
 
