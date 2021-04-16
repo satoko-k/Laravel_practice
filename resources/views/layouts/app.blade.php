a626f6cebb268a5669f7759cfbc763b0fc6bd5fc
satoko-k <80142169+satoko-k@users.noreply.github.com>
2021-04-17 06:45:05 +0900

    Add files via upload

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!-- resources/views/layouts/app.blade.php -->
+
+<!DOCTYPE html>
+<html lang ="ja">
+    <head>
+        <title>Book List</title>
+        
+        <!-- CSSとjs--->
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        
+    </head>
+    
+    <body>
+        <div class="container">
+            <nav class="navbar navbor-default">
+                <!-－ナビバーの内容-->
+        </div>
+        
+        @yield('content')
+        
+    
+        
+    </body>
+    
+    
+    
+    
+</html>
